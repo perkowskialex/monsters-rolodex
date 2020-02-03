523b1cc8d017da0c4598a36a47078a5a7ebbe4e9
Alex Perkowski <perkowskialex@gmail.com>
2020-02-03 16:50:31 -0600

    fix repo

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      // case insensitive
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => {
+            this.setState({ searchField: e.target.value });
+          }}
+        ></input>
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
